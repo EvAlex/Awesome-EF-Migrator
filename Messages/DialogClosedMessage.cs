a656bdf35972931478a2b3094748d7303e95c159
EvAlex <evalexhimself@gmail.com>
2015-02-06 16:13:29 +0300

    AQ-7 DbInitializer connect dialog open-close

---
+++ Messages/DialogClosedMessage.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PoliceSoft.Aquas.Model.Initializer.Messages
+{
+	public class DialogClosedMessage<TDialog>
+	{
+	}
+}
