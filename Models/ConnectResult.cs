020c6724857441ddc7f51a62bf99b6e3003de8b1
EvAlex <evalexhimself@gmail.com>
2015-02-06 17:58:35 +0300

    AQ-7 DbInitializer connect dialog error dialog

---
+++ Models/ConnectResult.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PoliceSoft.Aquas.Model.Initializer.Models
+{
+	public class ConnectResult
+	{
+		public ConnectResult(Exception exception)
+		{
+			this.exception = exception;
+			this.success = exception == null;
+        }
+
+		public Exception Exception
+		{
+			get { return exception; }
+		}
+		private readonly Exception exception;
+
+		public bool Success
+		{
+			get { return success; }
+		}
+		private readonly bool success;
+	}
+}
---
+++ Views/IDialog.cs
@@ -0,0 +1,9 @@
+﻿namespace PoliceSoft.Aquas.Model.Initializer.Views
+{
+	public interface IDialog : IWindow
+	{
+		bool? ShowDialog();
+
+		void Close();
+	}
+}
