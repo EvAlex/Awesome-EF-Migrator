b9c6f1fe155c87d7e33e0f64bbeadabadec96353
EvAlex <evalexhimself@gmail.com>
2015-02-13 00:46:14 +0300

    added sln file and moved project under src/Migrator dir

--- src/Migrator/Properties/Resources.Designer.cs
+++ src/Migrator/Properties/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace PoliceSoft.Aquas.Model.Initializer.Properties {
+namespace EvAlex.AwesomeEfMigrator.Properties {
     using System;
     
     
@@ -39,7 +39,7 @@ namespace PoliceSoft.Aquas.Model.Initializer.Properties {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("PoliceSoft.Aquas.Model.Initializer.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("EvAlex.AwesomeEfMigrator.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
