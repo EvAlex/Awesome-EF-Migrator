00376ef316db28faffa80d7a20cff160410636e3
EvAlex <evalexhimself@gmail.com>
2015-02-12 09:35:18 +0300

    DbInitializer is now DbMigrator. DbMigrator: removed references to other projects, added config

---
+++ ZeroConfig.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PoliceSoft.Aquas.Model.Initializer
+{
+	internal sealed class ZeroConfig : AwesomeEfMigratorConfig
+	{
+		private readonly Type dbContextType;
+
+        public ZeroConfig(Type dbContextType)
+		{
+			this.dbContextType = dbContextType;
+        }
+
+		protected override void Configure(AwesomeEfMigratorConfigBuilder configBuilder)
+		{
+			configBuilder.UseDbContext(dbContextType);
+        }
+	}
+}
