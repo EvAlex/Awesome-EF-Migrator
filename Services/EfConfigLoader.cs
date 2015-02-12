380cba7e60a2c5ae965be7597bedd3b6558180b4
EvAlex <evalexhimself@gmail.com>
2015-02-12 23:41:13 +0300

    DbMigrator: removed hard-coded path to data.storage.dll

--- Services/EfConfigLoader.cs
+++ Services/EfConfigLoader.cs
@@ -11,14 +11,18 @@ namespace PoliceSoft.Aquas.Model.Initializer.Services
 {
 	public class EfConfigLoader : IEfConfigLoader
 	{
-		private readonly string[] targetAssembliesPaths = new string[]
-			{
-				Path.Combine(Directory.GetCurrentDirectory(), @"..\..\..\Data.Storage\bin\debug\PoliceSoft.Aquas.Data.Storage.dll"),
-			};
+		private const string MigrationAssembliesDir = "MigrationAssemblies";
+
+		//private readonly string[] targetAssembliesPaths = new string[]
+		//	{
+		//		Path.Combine(Directory.GetCurrentDirectory(), @"..\..\..\Data.Storage\bin\debug\PoliceSoft.Aquas.Data.Storage.dll"),
+		//	};
 
 		public IEnumerable<AwesomeEfMigratorConfig> LoadConfigs()
 		{
-			var assemblies = targetAssembliesPaths.Select(path => Assembly.LoadFile(path));
+			string dir = Path.Combine(Directory.GetCurrentDirectory(), MigrationAssembliesDir);
+			var targetAssembliesPaths = Directory.GetFiles(dir, "*.dll").Union(Directory.GetFiles(dir, "*.exe"));
+            var assemblies = targetAssembliesPaths.Select(path => Assembly.LoadFile(path));
 			foreach (var a in assemblies)
 			{
 				var types = a.GetTypes();
